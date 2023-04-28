fdee7bec055c87ab1ad740505759a7c5c78ef703
iorid3 <itakeshita@my.bcit.ca>
2023-04-27 22:59:58 -0700

    origin change

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 const cors = require('cors')
 app.use(cors({
-  origin: "http://localhost:3001",
+  origin: "https://todosback.onrender.com/",
   optionsSuccessStatus: 200
 }));
 
